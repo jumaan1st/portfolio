709b8a8e3f037fa7f60cc17c8403e4003b002405
jumaan1st <mohammedjumaan.mf@gmail.com>
2025-12-14 01:20:21 +0530

    feat: initialize PostgreSQL database connection pool using environment variables.

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,14 @@
 import { Pool } from 'pg';
 
-user: process.env.DB_USER,
+const pool = new Pool({
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-            port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-                database: process.env.DB_NAME,
-                    ssl: {
-    rejectUnauthorized: false
-}
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default pool;
